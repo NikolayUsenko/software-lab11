0ae681b4bf33ceb506397ad8e5b0196827295cfa
Николай <usenkko.nikolay@gmail.com>
2025-12-09 00:01:34 +0300

    Update for task 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,21 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace software_lab11
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            Binding bind = new Binding();
+            bind.ElementName = "Mark";
+            bind.Path = new PropertyPath("IsChecked");
+            bind.Mode = BindingMode.Default;
+            Target.SetBinding(TextBox.IsReadOnlyProperty, bind);
         }
     }
 }
